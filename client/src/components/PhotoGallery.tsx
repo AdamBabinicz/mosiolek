6187f1c1a52edaaa4aa257ca1e3c2228f2ac17d3
babinicz <babinicz@onet.pl>
2025-04-08 12:22:45 +0200

    text-gray

--- client/src/components/PhotoGallery.tsx
+++ client/src/components/PhotoGallery.tsx
@@ -110,7 +110,7 @@ const PhotoGallery = () => {
                   <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                     {photo.caption}
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                  <p className="mt-1 text-sm text-gray-700 dark:text-gray-300">
                     {photo.description}
                   </p>
                 </div>
